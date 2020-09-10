4ec19e920872aa313a735aad3df899f46a0fdc54
Tiffini <tiffini.baldwin@gmail.com>
2020-09-09 21:37:14 -0400

    Created a new repo for the MTA course
    Did some hello world stuff. Learned more about if/else and switch statements.

--- Lesson01/IntroducingCS/Program.cs
+++ Lesson01/IntroducingCS/Program.cs
@@ -7,6 +7,16 @@ namespace IntroducingCS
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int x = 10;
+            x++;
+            ++x;
+            Console.WriteLine($"X equals {x}");
+
+            int y = x++;
+            Console.WriteLine($"Y equals {y}");
+
+            int z = ++x;
+            Console.WriteLine($"Z equals {z}");
         }
     }
 }
