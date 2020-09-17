ca9da114095ae247096249e84f14e28891fb51b3
Tiffini <tiffini.baldwin@gmail.com>
2020-09-16 20:22:03 -0400

    Recursive method
    Learned about the recursive method

---
+++ Lesson01/RecursiveFactorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factorial(5);
+        }
+        public static int Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1; //base case
+            }
+            else
+            {
+                return n * Factorial(n - 1); //recursive case
+            }
+        }
+    }
+}
