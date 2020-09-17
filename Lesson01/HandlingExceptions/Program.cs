91bf64efa5199b5cd814392ef3fecb6b5f4092c3
Tiffini Baldwin <33402990+iniffit@users.noreply.github.com>
2020-09-17 07:50:40 -0400

    Exceptions
    Learning about throwing and catching exceptions

---
+++ Lesson01/HandlingExceptions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExceptionTest();
+        }
+        private static void ExceptionTest()
+        {
+            StreamReader sr = null;
+            try
+            {
+                sr = File.OpenText(@"c:\Users\tiff\Documents\data.txt");
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            catch (FileNotFoundException fnfe)
+            {
+                Console.WriteLine(fnfe.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
