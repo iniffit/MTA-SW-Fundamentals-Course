1c566529acec58fa1691f3fd1153074946785b9a
Tiffini <tiffini.baldwin@gmail.com>
2020-09-26 00:12:13 -0400

    Started Lesson 2
    So far, we discussing objects and classes.

---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Rectangle
+    {
+        private double length;
+        private double width;
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+    }
+}
